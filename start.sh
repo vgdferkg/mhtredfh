c90e818283cdec33520bec74005c17fde6982194
vgdferkg <153043938+vgdferkg@users.noreply.github.com>
2023-12-06 02:10:21 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 export UUID=e4c40e74-8352-4bba-a36c-10349334eba4
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=techtanker.com
+export ProxySite=boredgamecompany.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
